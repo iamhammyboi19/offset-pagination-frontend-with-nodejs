9729df5d10f31d2d38293f35c1d5976087e7cd15
iamhammyboi19 <88974019+iamhammyboi19@users.noreply.github.com>
2022-12-10 14:26:16 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,92 +1,156 @@
-// THIS SHOULD BE OUTSIDE AND SEPERATED FROM THE CODE BELOW starting from let nowCur
-const currentPage = 1;
-
+// set current page
 const setCurPage = (curpage) => sessionStorage.setItem("page", curpage);
 
+// set prev and next page
+const setCurNextPage = (thepage) => sessionStorage.setItem("nextpage", thepage);
+const setCurPrevPage = (thepage) => sessionStorage.setItem("prevpage", thepage);
+
+// functions to get pages saved to the sessionStorage
 const getCurPage = () => +sessionStorage.getItem("page") || 1;
+const getCurNextPage = () => +sessionStorage.getItem("nextpage") || 1;
+const getCurPrevPage = () => +sessionStorage.getItem("prevpage") || 1;
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// set previous and next page href to the sessionStorage eg www.me.com/?page=1, www.me.com/?page=3
+// NOTE you're storing the numbers coming from code line 9-10
+// I do this because the page refreshes when you click on links 
+// and on window('load',()=>{}) I set href attribute by geting the setPrevLink and setNextLink...
+// ...to something like this www.me.com/?page=1
+const setPrevLink = (prev) => sessionStorage.setItem("prevv", prev);
+const setNextLink = (next) => sessionStorage.setItem("nextt", next);
 
-let nowCur = +getCurPage();
+let currentPage;
+let nextpage;
+let prevpage;
 
-// get all contents on each page 
-// if the API is limited to bringing 10 results from the server get the dom element for all the pages available at the moment
- const getAllTransacionsTable = document.querySelectorAll(
-    ".trans-table-content"
-  );
+// get current page
+currentPage = getCurPage();
 
-  // first page and others
-  if (
-    nowCur === 1 &&
-    allPaymentResults > 1 &&
-    [...getAllTransacionsTable].length === 10
-  ) {
-    let getcur = +getCurPage();
-    getcur++;
+// I make the pages set themselves on first and last 
+// I use clicking event on prev and next button to increment or decrement previous and next page in the sessionStorage
+// and I use e.preventDefault() to make it work
+
+  // page 1 and no other page
+  if (currentPage === 1 && allPaymentResults === 1) {
+    if (
+      document.querySelector(".next-page") &&
+      document.querySelector(".prev-page")
+    ) {
+      document.querySelector(".next-page").style["pointer-events"] = "none";
+      document.querySelector(".prev-page").style["pointer-events"] = "none";
+    }
+  }
+
+  // page 1 and other page
+  if (currentPage === 1 && allPaymentResults > 1) {
+    nextpage = currentPage + 1;
+    setCurNextPage(nextpage);
+    setCurPage(1);
+
     document
       .querySelector(".next-page")
-      ?.setAttribute("href", `${secondss}page=${getcur}`);
-    setCurPage(getcur);
-    console.log("first case 2", getCurPage());
-    
-    document.querySelector(".prev-page").style["pointer-events"] = "none";
+      ?.setAttribute("href", `${secondss}page=${nextpage}`);
 
-    // last page
-  } else if (
-    nowCur === allPaymentResults &&
-    allPaymentResults > 1 &&
-    [...getAllTransacionsTable].length < 10
-  ) {
-    console.log("second case");
-    let getcur = +getCurPage();
-    getcur--;
-    document
-      .querySelector(".prev-page")
-      ?.setAttribute("href", `${secondss}page=${getcur}`);
-    setCurPage(getcur);
-    document.querySelector(".next-page").style["pointer-events"] = "none";
+    setNextLink(`${secondss}page=${nextpage}`);
 
-    // any other pages that is not first or last
-  } else if (
-    nowCur < allPaymentResults &&
-    [...getAllTransacionsTable].length === 10
-  ) {
-    console.log("third case");
-    let getcur = +getCurPage();
-    getcur++;
-    document
-      .querySelector(".next-page")
-      ?.setAttribute("href", `${secondss}page=${getcur}`);
-    // getcur--;
-    setCurPage(getcur);
-
-    let getcur2 = +getCurPage();
-    getcur2 = getcur2 - 2;
-    document
-      .querySelector(".prev-page")
-      ?.setAttribute("href", `${secondss}page=${getcur2}`);
-    // setCurPage(getcur);
-    document.querySelector(".next-page").style["pointer-events"] = "visible";
-    document.querySelector(".prev-page").style["pointer-events"] = "visible";
-    
-//     only one page
-  } else if (nowCur === 1 && allPaymentResults === 1) {
-    console.log("last case");
-    document.querySelector(".next-page").style["pointer-events"] = "none";
-    document.querySelector(".prev-page").style["pointer-events"] = "none";
-    
-//     when you go back to the first page you want to reinitialize the whole event
-  } else if (
-    nowCur === allPaymentResults &&
-    [...getAllTransacionsTable].length === 10
-  ) {
-    nowCur = 1;
-    let getcur = +getCurPage();
-    getcur--;
-    document
-      .querySelector(".next-page")
-      ?.setAttribute("href", `${secondss}page=${getcur}`);
-    setCurPage(getcur);
-    // console.log("first case 2", getCurPage());
     document.querySelector(".prev-page").style["pointer-events"] = "none";
   }
+
+  // last page
+  if (currentPage === allPaymentResults && allPaymentResults > 1) {
+    setCurPage(allPaymentResults);
+    currentPage = getCurPage();
+    prevpage = currentPage - 1;
+    setCurPrevPage(prevpage);
+
+    document
+      .querySelector(".prev-page")
+      ?.setAttribute("href", `${secondss}page=${prevpage}`);
+    setCurPage(prevpage);
+
+    setPrevLink(`${secondss}page=${prevpage}`);
+    setNextLink(`#`);
+    document.querySelector(".next-page").style["pointer-events"] = "none";
+  }
+
+  // any other page not first and last
+  document.querySelector(".next-page")?.addEventListener("click", function (e) {
+//    prevent page from reloading immediately you click the link
+    e.preventDefault();
+
+//    get the href which is set from sessionStorage on window load event and I load it after 1 second
+    const href = document.querySelector(".next-page").getAttribute("href");
+
+    if (currentPage <= allPaymentResults) {
+      currentPage = +getCurNextPage();
+      prevpage = currentPage - 1;
+      nextpage = currentPage + 1;
+      setCurPage(currentPage);
+      setCurPrevPage(prevpage);
+      setCurNextPage(nextpage);
+
+      document
+        .querySelector(".next-page")
+        ?.setAttribute("href", `${secondss}page=${nextpage}`);
+
+      setNextLink(`${secondss}page=${nextpage}`);
+
+      document
+        .querySelector(".prev-page")
+        ?.setAttribute("href", `${secondss}page=${prevpage}`);
+
+      setPrevLink(`${secondss}page=${prevpage}`);
+
+      /////////////////////////////////////////////////////////
+
+      document.querySelector(".next-page").style["pointer-events"] = "visible";
+      document.querySelector(".prev-page").style["pointer-events"] = "visible";
+    }
+
+    setTimeout(() => {
+      window.location.replace(href);
+    }, 1000);
+  });
+
+  document.querySelector(".prev-page")?.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = document.querySelector(".prev-page").getAttribute("href");
+
+    if (currentPage <= allPaymentResults) {
+      currentPage = +getCurPrevPage();
+      nextpage = currentPage + 1;
+      prevpage = currentPage - 1;
+
+      setCurPage(currentPage);
+      setCurNextPage(nextpage);
+      setCurPrevPage(prevpage);
+
+      document
+        .querySelector(".next-page")
+        ?.setAttribute("href", `${secondss}page=${nextpage}`);
+
+      setNextLink(`${secondss}page=${nextpage}`);
+
+      document
+        .querySelector(".prev-page")
+        ?.setAttribute("href", `${secondss}page=${prevpage}`);
+      setPrevLink(`${secondss}page=${prevpage}`);
+
+      /////////////////////////////////////////////////////////
+
+      document.querySelector(".next-page").style["pointer-events"] = "visible";
+      document.querySelector(".prev-page").style["pointer-events"] = "visible";
+    }
+
+    setTimeout(() => {
+      window.location.replace(href);
+    }, 1000);
+  });
+
+// get the previous page link and next page link from sessionStorage on reload
+  window.addEventListener("load", (e) => {
+    const getnext = sessionStorage.getItem("nextt") || "#";
+    const getprev = sessionStorage.getItem("prevv") || "#";
+    document.querySelector(".next-page")?.setAttribute("href", getnext);
+    document.querySelector(".prev-page")?.setAttribute("href", getprev);
+  });
